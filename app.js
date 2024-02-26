213d8c62797e49ecf45ed433f9ab0f0e4f767420
mfikriam <yapkun17@gmail.com>
2024-02-27 05:02:55 +0800

    add mysql conn

---
+++ app.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+// const kecelakaanRouter = require('./routes/kecelakaanRoutes');
+// const viewRouter = require('./routes/viewRoutes');
+
+// Start express app
+const app = express();
+
+// Implement CORS
+app.use(cors());
+app.options('*', cors());
+
+// Using pug view engine
+app.set('view engine', 'pug');
+
+//  Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Body parser, reading data from body into req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+// app.use('/', viewRouter);
+
+// Not Found Routes
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// ? Error Handler/Middleware
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ models/pengajuModel.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+
+const pengajuSchema = {
+  jabatan: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  masa_kerja: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  grade_gaji: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  status_nikah: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  jumlah_anak: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  hutang_tempat_lain: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  prediksi_kelayakan: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  akurasi: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+};
+
+module.exports = pengajuSchema;
