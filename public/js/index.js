278d22f4d6dcd79fcbc9f0b86b258537c0fa5c01
mfikriam <yapkun17@gmail.com>
2024-02-27 22:16:10 +0800

    add alert

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,8 @@
 import '@babel/polyfill';
 import { DataTable } from 'simple-datatables';
 
+import { showAlert } from './alert';
+
 //? DOM Element - Halaman Pegawai
 const pegawaiTable = document.querySelector('#pegawai-table');
 
@@ -13,4 +15,13 @@ if (pegawaiTable) {
     columns: [{ select: 8, sortable: false }],
   };
   new DataTable(pegawaiTable, options);
+}
+
+//************************** ALERT ********************************** */
+const delayAlertMsg = sessionStorage.getItem('delay-alert-message');
+const delayAlertType = sessionStorage.getItem('delay-alert-type');
+if (delayAlertMsg) {
+  showAlert(delayAlertMsg, delayAlertType);
+  sessionStorage.removeItem('delay-alert-message');
+  sessionStorage.removeItem('delay-alert-type');
 }
