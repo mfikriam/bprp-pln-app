652b5ed0c3a95c7d3d4a57e15b5b47209a9a347e
mfikriam <yapkun17@gmail.com>
2024-02-27 12:14:29 +0800

    add read all data pegawai

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,17 +1,30 @@
+// const { Pegawai, Pengaju } = require('./../models');
+const { Pegawai } = require('./../models');
+const catchAsync = require('./../utils/catchAsync');
+
+//**************************** EXPORTED FUNCTIONS *********************************/
 exports.getBlankPage = (req, res) => {
   res.status(200).render('blank', {
     title: 'Blank Page',
   });
 };
 
+exports.getPegawaiPage = catchAsync(async (req, res, next) => {
+  const resultQuery = await Pegawai.findAll({
+    order: [['nama', 'ASC']],
+  });
+  const resultQueryArr = resultQuery.map((instance) => instance.dataValues);
+
+  res.status(200).render('pegawai', {
+    title: 'Halaman Pegawai',
+    modelName: 'pegawai',
+    pegawaiObjArr: resultQueryArr,
+  });
+});
+
 exports.getPengajuPage = (req, res) => {
   res.status(200).render('pengaju', {
     title: 'Halaman Pengaju',
-  });
-};
-
-exports.getPegawaiPage = (req, res) => {
-  res.status(200).render('pegawai', {
-    title: 'Halaman Pegawai',
+    modelName: 'pengaju',
   });
 };
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,16 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { DataTable } from 'simple-datatables';
+
+//? DOM Element - Halaman Pegawai
+const pegawaiTable = document.querySelector('#pegawai-table');
+
+//***************** Halaman Pegawai ********************/
+//? Datatables
+if (pegawaiTable) {
+  const options = {
+    perPage: 5,
+    columns: [{ select: 8, sortable: false }],
+  };
+  new DataTable(pegawaiTable, options);
+}
