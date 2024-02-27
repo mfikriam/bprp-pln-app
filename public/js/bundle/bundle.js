6b404cd0baed69798e640774f58534e16376f172
mfikriam <yapkun17@gmail.com>
2024-02-27 22:05:16 +0800

    change logo height

--- public/js/bundle/bundle.js
+++ public/js/bundle/bundle.js
@@ -9945,7 +9945,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49718" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59266" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
